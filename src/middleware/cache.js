d2424d1e775755ccff336f4871b0808681420328
AzimIndiit <mohdazimindiit@gmail.com>
2025-06-29 13:52:52 +0530

    Update cache.js

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   if (req.method === 'GET' && !req.originalUrl.startsWith('/api/v1/auth')) {
-    res.setHeader('Cache-Control', 'public, max-age=300'); // cache for 5 minutes
+    res.setHeader('Cache-Control', 'no-store'); // cache for 5 minutes
   } else {
     res.setHeader('Cache-Control', 'no-store'); // do not cache auth or sensitive routes
   }
